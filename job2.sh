d1649ba1eaac561891964d0031d348ff590df259
ArudiBa <13911151653@163.com>
2025-07-12 15:43:29 +0800

    Create job2.sh

---
+++ job2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --job-name=test01
+#SBATCH --partition=gpu4090
+#SBATCH --qos=4gpus
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus=2
+#SBATCH --time=02:00:00
+#SBATCH --output=%j.out
+#SBATCH --error=%j.e
+
+module purge
+module load anaconda3
+source activate pytorch_env
+
+export CUDA_LAUNCH_BLOCKING=1
+
+python ./run_eval.py
